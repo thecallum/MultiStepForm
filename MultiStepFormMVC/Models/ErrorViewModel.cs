2505b0985f0db5ff1e983098a8d3aa51e30dcd09
Callum Macpherson <callummac@protonmail.com>
2021-04-19 14:02:56 +0100

    Add project files.

---
+++ MultiStepFormMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultiStepFormMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
